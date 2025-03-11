0e917cee1b4e97cb9628ccec22ef07b1df11af6f
si.wu <si.wu@appshahe.com>
2025-03-11 15:12:18 +0800

    pc 软连接

--- build_wasm.sh
+++ build_wasm.sh
@@ -5,5 +5,11 @@ cargo build --release --target wasm32-unknown-unknown -p crab_feast_pc
 # generate js
 wasm-bindgen --out-name crab_feast_wasm --out-dir launcher/wasm/target --target web target/wasm32-unknown-unknown/release/crab_feast_pc.wasm
 
+# assets copy
+
+# powershell
+# New-Item -ItemType SymbolicLink -Path "./assets" -Target "../../assets"
+
+
 # run
 http-server launcher/wasm --cors -p 8080
